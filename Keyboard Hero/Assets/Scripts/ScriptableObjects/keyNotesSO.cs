ff561337bb6309fa8770aaa20e52eaa6f91a6fb8
Enes <enescubuk10@gmail.com>
2022-11-27 07:16:19 +0300

    keynoteso

--- Keyboard Hero/Assets/Scripts/ScriptableObjects/keyNotesSO.cs
+++ Keyboard Hero/Assets/Scripts/ScriptableObjects/keyNotesSO.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName ="Music/Note Keys" , fileName = "your_music_name_notes")]
-public class keyNotesSO : MonoBehaviour
+public class keyNotesSO : ScriptableObject
 {
-    public List<int> Keys = new List<int>();
+    public List<int> NoteKeys = new List<int>();
+    public List<GameObject> keyPrefabs = new List<GameObject>();
 }
